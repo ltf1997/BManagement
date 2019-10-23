64728581cde483ef6ad6ad8f0ff8225540e3a395
ltf1997 <54401613+ltf1997@users.noreply.github.com>
2019-10-23 15:04:01 +0800

    'lala'

--- bm/src/component/nav/index.js
+++ bm/src/component/nav/index.js
@@ -15,47 +15,39 @@ class Nav extends Component {
           defaultOpenKeys={['sub1']}
           style={{ height: '100%', borderRight: 0 }}
         >
+          
+            <Menu.Item key="1"
+            title={
+              <span>
+                <Icon type="user" />
+                首页
+              </span>
+            }>首页</Menu.Item>
+          
           <SubMenu
             key="sub1"
             title={
               <span>
-                <Icon type="user" />
-                subnav 1
+                <Icon type="laptop" />
+                订单审核
               </span>
             }
           >
-            <Menu.Item key="1">首页</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
+            <Menu.Item key="5">查看全部</Menu.Item>
+            <Menu.Item key="6">添加订单</Menu.Item>
+            <Menu.Item key="7">未通过</Menu.Item>
           </SubMenu>
           <SubMenu
             key="sub2"
             title={
               <span>
-                <Icon type="laptop" />
-                subnav 2
-              </span>
-            }
-          >
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub3"
-            title={
-              <span>
                 <Icon type="notification" />
-                subnav 3
+                客户管理
               </span>
             }
           >
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
+            <Menu.Item key="9">查看全部</Menu.Item>
+            <Menu.Item key="10">添加客户</Menu.Item>
           </SubMenu>
         </Menu>
       
